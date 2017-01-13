ed47017e1f704de1324e6614a3539474e6e14e5c
bumpas <bumpas@andrewbumpas.com>
2017-01-13 09:37:23 -0700

    remove comments from jetpack carousel

--- wp-content/themes/hanna-child/functions.php
+++ wp-content/themes/hanna-child/functions.php
@@ -8,4 +8,17 @@
 	}
 	
 	add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
+
+	/* ==========================================
+	   Remove comments from jetpack carousel
+	   ========================================== */
+
+	function tweakjp_rm_comments_att( $open, $post_id ) {
+    	$post = get_post( $post_id );
+    	if( $post->post_type == 'attachment' ) {
+        	return false;
+    	}
+    	return $open;
+	}
+	add_filter( 'comments_open', 'tweakjp_rm_comments_att', 10 , 2 );
 ?>
